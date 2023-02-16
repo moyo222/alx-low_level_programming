9f4a543948a1c3f7dd21efe4dbbb7fb9ceda0c4d
bankole moyo <bankolemoyo5@gmail.com>
2023-02-16 04:35:30 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,12 @@
 /**
  * main - prints "with proper grammar, but the outcome is a piece of art,"
  * Return: Always 0 
+ *
  */
 int main(void)
 
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
