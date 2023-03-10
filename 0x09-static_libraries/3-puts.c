6f6e4e2fe0406e4ff20477a3f25d3a506db64203
moyo222 <bankolemoyo5@gmail.com>
2023-03-10 05:55:42 -0800

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+* _puts - prints a string to stdout
+* @str: the string to be printed
+*/
+
+void _puts(char *str)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
