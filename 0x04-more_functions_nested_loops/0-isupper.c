2e68b5cb65c0c2e2ddf13e187a3d9f1045f3efc2
moyo222 <bankolemoyo5@gmail.com>
2023-06-19 10:54:27 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h" 
+  
+ /** 
+  * _isupper - This function checks for uppercase character 
+  * @c: This is the character to be checked 
+  * Return: 1 if c is uppercase, 0 otherwise 
+  */ 
+ int _isupper(int c) 
+ { 
+         return (c >= 'A' && c <= 'Z'); 
+ }
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h" 
+  
+ /** 
+  * _isdigit - This function checks for a digit (0 through 9) 
+  * @c: This is the int to be checked 
+  * Return: 1 if c is a digit, 0 otherwise 
+  */ 
+ int _isdigit(int c) 
+ { 
+         return (c >= '0' && c <= '9'); 
+ }
